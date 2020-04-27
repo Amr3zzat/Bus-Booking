674f9f51ab778cbf265279e5f84066c3a30eebaf
Amr Ezzat <3mr3zzat@gmail.com>
2020-04-27 22:20:22 +0200

    [Routes] Add Api Endpoints

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/seats', 'ShowAvailableSeatsAction@index');
+Route::post('/seats', 'BusBookingAction@reserve');
+Route::get('/buses', 'ShowBusesAction@index');
