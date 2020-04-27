a96472dcb6e74fa81ff916f58f4ecd7fe649021d
Amr Ezzat <3mr3zzat@gmail.com>
2020-04-27 22:13:09 +0200

    [DI] Inject BusRepository

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repository\BusRepository;
+use App\Repository\BusRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->app->bind(BusRepositoryInterface::class, BusRepository::class);
     }
 }
